b39f5b95687f68b1822ad0a178951662bc432fa1
umg <umg@JOC-LAB-1F4.labjoc.local>
2026-04-14 19:09:38 -0600

    clase empleado, se agrego un metodo para calcular salario

--- src/githubumg/empleado.java
+++ src/githubumg/empleado.java
@@ -5,53 +5,56 @@ public class empleado {
     String nombre;
     String apellido;
     String direccion;
+    float salario;
 
     public empleado() {
         this.id = 0;
         this.nombre = "";
         this.apellido = "";
         this.direccion = "";
+        this.salario=0;
     }
-
-    public empleado(int id, String nombre, String apellido, String direccion) {
+    public empleado(int id, String nombre, String apellido, String direccion,float salario) {
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
         this.direccion = direccion;
+        this.salario=salario;
     }
-
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
     public String getApellido() {
         return apellido;
     }
-
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
-
     public String getDireccion() {
         return direccion;
     }
-
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
+    public float getSalario() {
+        return salario;
+    }
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
     
-    
-    
+    public float calcularsalario(){
+        salario=salario*5;
+        return salario;
+    }
 }
